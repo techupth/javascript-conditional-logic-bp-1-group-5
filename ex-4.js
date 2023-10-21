822a2982db502766430c1846799633f08864a8d7
Cynthy YD <cynthyyd@Cynthys-MacBook-Pro.local>
2023-10-21 12:10:42 +0700

    add: exercise answers 1-7

--- ex-4.js
+++ ex-4.js
@@ -3,3 +3,18 @@
 let lightBulbStatus = "On";
 
 //Start coding here
+lightBulbStatus = "Broken";
+
+switch (lightBulbStatus){
+    case "On":
+        console.log("Light bulb is On.");
+    break;
+    case "Off":
+        console.log("Light bulb is Off.");
+    break;
+    case "Broken":
+        console.log("Light bulb is Broken.");
+    break;
+    default:
+        console.log("Please choose the correct input (On/Off/Broken)");
+}
